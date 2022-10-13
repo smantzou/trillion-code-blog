310b24213d2a453a109d45c2bfd91a97d3118f8b
smantzou <steliosmantzouranis@gmail.com>
2022-10-13 23:22:19 +0300

    tailwind added

--- trillion-code-blog-app/src/main.ts
+++ trillion-code-blog-app/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { router } from "./router";
+import "./main.css";
 import App from "./App.vue";
 
 createApp(App).use(router).use(createPinia()).mount("#app");
