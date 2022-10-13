dff876c2d9fa78e09c6f068bc5ab8a23438b486d
smantzou <steliosmantzouranis@gmail.com>
2022-10-13 21:15:52 +0300

    Blog Store done

--- trillion-code-blog-app/src/store/blog.ts
+++ trillion-code-blog-app/src/store/blog.ts
@@ -1,14 +1,22 @@
 import { defineStore } from "pinia";
-import axios, { Axios, AxiosError, AxiosResponse } from "axios";
 
 import { blogItem } from "@/interfaces/blogItem";
 import { backend } from "@/environment";
+import { Blog } from "@/interfaces/blog";
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+interface State {
+  blogs: blogItem[];
+  selectedBlogId: number;
+  selectedBlog: Blog | null;
+  error: AxiosError;
+}
 
 export const useBlogStore = defineStore("counter", {
-  state: () => ({
-    blogs: new Map<string, blogItem>(),
+  state: (): State => ({
+    blogs: [],
     selectedBlogId: 0,
-    selectedBlog: {},
+    selectedBlog: null,
     error: new AxiosError(),
   }),
   getters: {
@@ -22,9 +30,30 @@ export const useBlogStore = defineStore("counter", {
         const response: AxiosResponse<blogItem[]> = await axios.get(
           `${backend}/blogs?limit=${limit}&page=${page}`
         );
-        response.data.forEach((datum: blogItem) => {
-          this.blogs.set(datum.slug, datum);
-        });
+        this.blogs = response.data;
+      } catch (error) {
+        const axiosError = error as AxiosError;
+        if (axiosError.response) {
+          this.error = new AxiosError(
+            axiosError.response.statusText,
+            axiosError.response.status.toString()
+          );
+        } else {
+          console.log("Error", axiosError.message);
+          const status = axiosError.status ? axiosError.status : 500;
+          this.error = new AxiosError(
+            status.toString(),
+            axiosError.message.toString()
+          );
+        }
+      }
+    },
+    async fetchBlogBySlugWithRelatedBlogs(slug: string) {
+      try {
+        const response: AxiosResponse<Blog> = await axios.get(
+          `${backend}/blog/${slug}?includeRelatedBlogs=true&includeContent=true`
+        );
+        this.selectedBlog = response.data;
       } catch (error) {
         const axiosError = error as AxiosError;
         if (axiosError.response) {
